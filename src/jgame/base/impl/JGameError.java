afde80c491694ca2dce4145143b4dc6991e646cb
Sean Kau <sean@kautown.com>
2011-05-09 17:07:15 -0700

    Repackaged JGame more to my liking

---
+++ src/jgame/base/impl/JGameError.java
@@ -0,0 +1,21 @@
+package jgame.base.impl;
+
+/** Exception used for passing error conditions within JGame */
+public class JGameError extends Error {
+	String msg;
+	boolean fatal=false;
+
+	/** Construct non-fatal error */
+	public JGameError(String message) {
+		msg=message;
+	}
+
+	/** Construct fatal or non-fatal error */
+	public JGameError(String message, boolean fatal) {
+		msg=message;
+		this.fatal=fatal;
+	}
+
+	@Override
+	public String toString() { return msg; }
+}
